9bfca92d66ad187922a4e5ceb97cfba4d50ff5f4
Unknown <at.frizzy@gmail.com>
2018-11-24 20:38:29 +0100

    signatures fonctions productController

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Product;
+use Illuminate\View\View;
 
 class ProductController extends Controller
 {
@@ -15,6 +16,50 @@ class ProductController extends Controller
      */
     public function show($id) {
         return view('product.show', ['product' => Product::findOrFail($id)]);
+    }
+
+    /**
+     * Show the page to update a product identified by id
+     *
+     * @param $id
+     * @return View
+     */
+    public function update($id) {
+        return view('product.update', ['action' => 'update','product' => Product::findOrFail($id)]);
+    }
+
+    /**
+     * Handle the update of a product identified by id
+     *
+     * @param $id
+     */
+    public function updated($id) {
+
+    }
+
+    /**
+     * Show the page to create a new product
+     * @return View
+     */
+    public function create() {
+        return view('product.update', ['action' => 'create']);
+    }
+
+
+    /**
+     * Handle the creation of a new product
+     */
+    public function created() {
+
+    }
+
+    /**
+     * Delete the product identified by id
+     *
+     * @param $id
+     */
+    public function delete($id) {
+
     }
 
 }
