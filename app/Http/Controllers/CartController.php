570778aa0bd7c27c5a279bb6cdb8e3d14db06189
bejaouij <bejaoui.je@gmail.com>
2018-12-10 05:25:43 +0100

    - Cart item quantity view

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Cart;
+use App\Copy;
+use App\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -10,8 +12,20 @@ class CartController extends Controller
 {
     public function index() {
     	$cartItems = Cart::where('customer_id', Auth::user()->user_id)->get();
+    	$itemSummaries = [];
 
-    	return view('cart', compact(['cartItems']));
+    	foreach($cartItems as $cartItem) {
+    		$cartCopy = Copy::find($cartItem->copy_id);
+    		$cartProduct = Product::find($cartCopy->product_id);
+
+    		$itemSummaries[] = [
+    			'cart_copy' => $cartCopy,
+    			'cart_product' => $cartProduct,
+    			'cart_quantity' => $cartItem->cart_quantity
+    		];
+    	}
+
+    	return view('cart', compact(['itemSummaries']));
     }
 
     /**
--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -2,8 +2,14 @@
 
 @section('content')
 <div class="container">
-	@forelse($cartItems as $cartItem)
-		{{ $cartItem->customer_id }}
+	@forelse($itemSummaries as $itemSummary)
+		<div class="cart-item">
+			<h2>{{ $itemSummary['cart_product']->product_name }}</h2>
+			<p>Version: {{ $itemSummary['cart_copy']->copy_name }}</p>
+			<div>
+				<button class="cart-quantity" onclick="if(this.nextSibling.value > 0) this.nextSibling.value -= 1;">-</button><input class="cart-quantity" value="{{ $itemSummary['cart_quantity'] }}"><button class="cart-quantity" onclick="this.previousSibling.value += 1;">+</button>
+			</div>
+		</div>
 	@empty
 		Your cart is empty.
 	@endforelse
