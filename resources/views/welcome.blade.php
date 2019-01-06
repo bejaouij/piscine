f5886312590a111cd65661d715e13a527e24fca4
bejaouij <bejaoui.je@mail.com>
2019-01-06 18:32:30 +0100

    style($view): comment side nav
    Comment the home page side nav.
    
    Before: display side nav.
    After: hide side nav.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 @section('content')
-    <ul id="slide-out" class="sidenav sidenav-fixed">
-        <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
-        <li><a href="#!">Second Link</a></li>
-        <li><div class="divider"></div></li>
-        <li><a class="subheader">Subheader</a></li>
-        <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
-    </ul>
+    {{--<ul id="slide-out" class="sidenav sidenav-fixed">--}}
+        {{--<li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>--}}
+        {{--<li><a href="#!">Second Link</a></li>--}}
+        {{--<li><div class="divider"></div></li>--}}
+        {{--<li><a class="subheader">Subheader</a></li>--}}
+        {{--<li><a class="waves-effect" href="#!">Third Link With Waves</a></li>--}}
+    {{--</ul>--}}
 
     <div class="row">
         <nav class="styleSearchBar z-depth-1">
