47fe6c0c4c10141a4677281cd05c5d3b0714a9cd
bejaouij <bejaoui.je@mail.com>
2019-01-08 03:34:44 +0100

    style($view): display product discount prices
    Display product percentage and both prices.
    
    Before: display only the intial price.
    After: diplay both the initial price and the discounted price.

--- resources/views/shop/show.blade.php
+++ resources/views/shop/show.blade.php
@@ -19,28 +19,34 @@
 
     <hr>
 
-    <div class="products-container">
+    <div class="products-container row">
         @forelse($shop->products as $product)
-            <div class="row">
-                <div class="product col s4">
-                    <div class="card-panel grey lighten-5 z-depth-1">
-                        <div class="row valign-wrapper">
-                            <div class="col s2">
-                                <img src={{ asset('media') . '/' . $product->photo_path->photo_relative_path }} alt="" class="responsive-img"> <!-- notice the "circle" class -->
-                            </div>
+            <div class="product col s4">
+                <div class="card-panel grey lighten-5 z-depth-1">
+                    <div class="row valign-wrapper">
+                        <div class="col s2">
+                            <img src={{ asset('media') . '/' . $product->photo_path->photo_relative_path }} alt="" class="responsive-img"> <!-- notice the "circle" class -->
+                        </div>
 
-                            <div class="col s8">
-                                <div class="black-text">
-                                    <p>{{ $product->product_name }}</p>
+                        <div class="col s8">
+                            <div class="black-text">
+                                <P>{{ $product->product_name }}</P>
+
+                                @if(empty($product->product_discount_percentage))
                                     <p>{{ number_format($product->product_price, 2, ',', ' ') }} &euro; TTC</p>
-                                    <p><a class="waves-effect waves-light btn" href="{{ route('product', ['id' => $product->product_id ]) }}">Voir</a></p>
-                                </div>
+                                @else
+                                    <p>
+                                        <span class="initial-price">{{ number_format($product->product_price, 2, ',', ' ') }}</span> {{ number_format(((1 - $product->product_discount_percentage) * $product->product_price), 2, ',', ' ') }} &euro; TTC <span class="discount-percentage">{{ $product->product_discount_percentage * 100 }} %</span>
+                                    </p>
+
+                                @endif
+
+                                <p><a class="waves-effect waves-light btn" href="{{ route('product', ['id' => $product->product_id ]) }}">Voir</a></p>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-
         @empty
             <p>
                 Ce commerce n'a pas de produit en vente.
